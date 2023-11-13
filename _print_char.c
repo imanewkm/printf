9339308defb2944b8ab9bcea4fe63020c18c9fb6
capo-no-baka <141492876+capo-no-baka@users.noreply.github.com>
2023-11-13 12:47:24 -0800

    _print_char.c

---
+++ _print_char.c
@@ -0,0 +1,13 @@
+/**
+ * _print_char - function that prints a character
+ * @argument: the variadic argument from the printf function
+ * Return: amount of characters printed (one).
+**/
+int _print_char(va_list argument)
+{
+	char c = (char) va_arg(argument, int);
+
+	_putchar (character);
+
+	return (1);
+}
